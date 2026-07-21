93b5eda2e22e8c29a447f7d24a4a67905ae3fed8
gujaratharva021-lgtm <gujaratharva021@gmail.com>
2026-07-21 13:42:45 +0530

    Fix widget test to use correct app class

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
 import 'package:election/main.dart';
 
 void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ElectionAIApp());
+  testWidgets('App builds without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(const OneVoteApp());
+    await tester.pump();
   });
 }
