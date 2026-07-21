246b199c9a6b0f204d035090560494252ae528d9
gujaratharva021-lgtm <gujaratharva021@gmail.com>
2026-07-21 13:58:44 +0530

    Wrap test widget with required providers

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:election/main.dart';
 
 void main() {
   testWidgets('App builds without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(const OneVoteApp());
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => ThemeProvider()),
+          ChangeNotifierProvider(create: (_) => YearProvider()),
+        ],
+        child: const OneVoteApp(),
+      ),
+    );
     await tester.pump();
   });
 }
